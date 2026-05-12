cee5e1d2e16546cf55a2539631699b50b27dd07a
Ocelot <FinntheRaider@gmail.com>
2026-05-12 16:05:12 -0600

    Fix Fabric 1.21.5 not being published

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ stonecutter {
             versions("1.21.1", "1.21.2", "1.21.4", "1.21.5", "1.21.6", "1.21.9", "1.21.11", "26.1", "26.2-snapshot-1", "26.2-snapshot-3", "26.2-snapshot-6", "26.2-snapshot-7")
         }
         branch("fabric") {
-            versions("1.21.1", "1.21.2", "1.21.4", "1.21.6", "1.21.9", "1.21.11").buildscript("mapped.build.gradle.kts")
+            versions("1.21.1", "1.21.2", "1.21.4", "1.21.5", "1.21.6", "1.21.9", "1.21.11").buildscript("mapped.build.gradle.kts")
 //            versions("1.21.1", "1.21.2", "1.21.3", "1.21.4", "1.21.5", "1.21.6", "1.21.7", "1.21.8", "1.21.9", "1.21.10", "1.21.11").buildscript("mapped.build.gradle.kts")
             versions("26.1", "26.2-snapshot-1", "26.2-snapshot-3", "26.2-snapshot-6", "26.2-snapshot-7")
 //            versions("26.1", "26.2-snapshot-1", "26.2-snapshot-2", "26.2-snapshot-3", "26.2-snapshot-4", "26.2-snapshot-5", "26.2-snapshot-6", "26.2-snapshot-7")
